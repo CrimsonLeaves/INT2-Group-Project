7c4d2842be64e85785b153f7b9e0c3cf79a73123
CrimsonLeaves <109231078+CrimsonLeaves@users.noreply.github.com>
2023-05-04 17:34:54 +0100

    Calculates the accuracy of the validation data
    The program now prints how accuractly it can predict the validation data.

--- NeuralNet1.py
+++ NeuralNet1.py
@@ -48,7 +48,6 @@ for X, y in val_dataloader:
     break
 
 
-
 # Define model
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -76,25 +75,40 @@ class NeuralNetwork(nn.Module):
             #Use the NLLLoss function for logsoftmax.
             
         )
-
-    def forward_step(self, x):
-       loss = nn.NLLLoss()
-       img, label = batch
-       output = self.network(img)
-       #Calculate and return loss
-       return loss(output, label)
-    
+      
+        
+    #Training
+    def forward_step(self, batch):
+        lossFunc = nn.NLLLoss()
+        img, label = batch
+        output = self.network(img)
+        #Calculate and return loss
+        return lossFunc(output, label)
+     
     #Run through network without calculating loss.
+    #Highest or lowest value is prediection?
     def run_net(self, batch):
         img, label = batch
         #return prediction
         return self.network(img)
     
-    #Check if it was correct.
-    def correct(self, label, output):
-        #takes a batch as input
-        #outputs how many were correctly predicted.
-        pass 
+    
+#Check if it was correct.
+def correct(labels, output):
+    #takes a batch as input
+    correct = 0
+    length = labels.tolist()
+    for i in range(len(length)):
+        #Index of predicted value
+        prediected = torch.argmax(output[i])
+       
+        #Does it match the actual label?
+        if prediected==labels[i]:
+            correct += 1
+        
+    #outputs how many were correctly predicted.        
+    return correct
+
 
 model = NeuralNetwork()
 learningRate = 0.1
@@ -102,7 +116,7 @@ optimizer = optim.SGD(model.parameters(), lr=learningRate)
 trainingLosses = []
 
 #needs to be done for multiple epochs
-for epoch in range(1):
+for epoch in range(10):
     for batch in train_dataloader:
         loss = model.forward_step(batch)
         #record losses
@@ -118,8 +132,12 @@ for epoch in range(1):
     
 
 #validate the model using the validation dataset
+total_correct = 0
 for batch in val_dataloader:
     prediction = model.run_net(batch)
     #calculate how many were correct
-    print(prediction)
-   
+    img, labels = batch
+    answer = correct(labels, prediction)
+    total_correct += answer
+val_accuracy = (total_correct/1020) * 100  
+print(val_accuracy)
